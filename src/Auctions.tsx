9c3819d93128c2940891b11f2f49c00840894798
Sameer Khan <sameer_khan@student.uml.edu>
2018-11-08 22:42:18 -0500

    more additions

---
+++ src/Auctions.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import { ListAuctionsQuery, ListAuctionsQueryVariables } from './API';
+import { listAuctions } from './graphql/queries';
+import { AuctionCard } from './AuctionCard';
+
+export const Auctions = () => {
+  return (
+    <Query<ListAuctionsQuery, ListAuctionsQueryVariables>
+      query={gql(listAuctions)}
+      variables={{ limit: 5 }}
+    >
+      {({ data, loading }) =>
+        loading ||
+        !data ||
+        !data.listAuctions ||
+        !data.listAuctions.items ? null : (
+          <div
+            style={{
+              display: 'grid',
+              gridTemplateColumns: 'repeat(4, 1fr)',
+              gridGap: '10px',
+            }}
+          >
+            {data.listAuctions.items.map(
+              x =>
+                !x ? null : (
+                  <AuctionCard name={x.name} price={x.price} key={x.id} />
+                ),
+            )}
+          </div>
+        )
+      }
+    </Query>
+  );
+};
